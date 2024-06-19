755de3caf302711520c73fd8d24da01459e84744
KCUBE21 <138729734+KCUBE21@users.noreply.github.com>
2024-06-19 23:49:46 +0000

    Update 102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,5 +1,16 @@
 #!/usr/bin/node
 const fs = require('fs');
-const a = fs.readFileSync(process.argv[2], 'utf8');
-const b = fs.readFileSync(process.argv[3], 'utf8');
-fs.writeFileSync(process.argv[4], a + b);
+const path = require('path');
+
+if (process.argv.length !== 5) {
+  console.error('Usage: node concatFiles.js <file1> <file2> <destination>');
+  process.exit(1);
+}
+
+const file1Path = path.resolve(process.argv[2]);
+const file2Path = path.resolve(process.argv[3]);
+const destinationPath = path.resolve(process.argv[4]);
+const file1Content = fs.readFileSync(file1Path, 'utf8');
+const file2Content = fs.readFileSync(file2Path, 'utf8');
+const concatenatedContent = file1Content + file2Content;
+fs.writeFileSync(destinationPath, concatenatedContent);
